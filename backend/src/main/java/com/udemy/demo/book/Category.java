d30f6704669628cc527bc4f9858c3a1f1b74fd48
Sylvain Maestri <sylvainmaestri@gmail.com>
2021-02-04 21:00:02 +0100

    Section 4 - Spring DATA

--- backend/src/main/java/com/udemy/demo/book/Category.java
+++ backend/src/main/java/com/udemy/demo/book/Category.java
@@ -1,6 +1,25 @@
 package com.udemy.demo.book;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     private String label;
 
     public Category() {
--- backend/src/main/java/com/udemy/demo/user/User.java
+++ backend/src/main/java/com/udemy/demo/user/User.java
@@ -1,14 +1,23 @@
 package com.udemy.demo.user;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.Size;
 
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
     private String email;
 
-    @Size(min=2, max=25, message="Le prénom doit faire entre 5 et 25 caractères")
+    @Size(min = 2, max = 25, message = "Le prénom doit faire entre 5 et 25 caractères")
     private String firstName;
-    @Size(min=2, max=25, message="Le nom doit faire entre 5 et 25 caractères")
+    @Size(min = 2, max = 25, message = "Le nom doit faire entre 5 et 25 caractères")
     private String lastName;
     private String password;
 
@@ -51,6 +60,13 @@ public class User {
         this.password = password;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
 
 
