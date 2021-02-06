4d306e21fb8ecedf52aadaf618b114ee4dbcb293
Sylvain Maestri <sylvainmaestri@gmail.com>
2021-02-06 23:13:51 +0100

    Section 7 - React - Les fondamentaux
    Add book

---
+++ frontend/src/ListBooks.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import Book from './Book'
+
+export default class ListBooks extends React.Component {
+
+  constructor() {
+    super();
+    this.state = { books: [] }
+  }
+  componentDidMount() {
+    // TODO charger les livres disponibles a partir des APi back
+    this.setState({
+      books: [
+        {
+          title: "asterix",
+          category: "BD",
+        },
+
+        {
+          title: "tintin",
+          category: "BD",
+        }
+      ]
+    })
+  }
+
+  render() {
+
+    return (<div>
+      <h1>Livres diponibles</h1>
+      {this.state.books.length === 0 ? "Pas de livres disponibles" : null}
+      {this.state.books.map(book => (<div>
+        <Book title={book.title} category={book.category}></Book>
+        <button>Emprunter</button>
+      </div>))}
+      <br />
+      <button>Nouveau livre</button>
+    </div>)
+
+  }
+
+}
