7ed920f93261027fe5a9b5f9a1a24bd81633c285
Sylvain Maestri <sylvainmaestri@gmail.com>
2021-02-07 00:20:26 +0100

    Section 8 - Routing avec React-Router

---
+++ frontend/src/Header.js
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/myBooks">Mes livres</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/myBorrows">Mes emprunts</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/listBooks">Livres disponibles</Link>
+                    </li>
+                </ul>
+                <div>Bienvenue, ...</div>
+                <button variant="secondary">Se déconnecter</button>
+            </div>
+        </nav>
+
+
+
+    )
+}
