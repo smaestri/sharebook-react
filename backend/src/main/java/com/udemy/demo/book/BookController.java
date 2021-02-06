df181d27862499c19a7ffd7808fa0a352375a039
Sylvain Maestri <sylvainmaestri@gmail.com>
2021-02-06 20:13:21 +0100

    Section 5 - Spring SECURITY

--- backend/src/main/java/com/udemy/demo/book/BookController.java
+++ backend/src/main/java/com/udemy/demo/book/BookController.java
@@ -2,14 +2,17 @@ package com.udemy.demo.book;
 
 import com.udemy.demo.borrow.Borrow;
 import com.udemy.demo.borrow.BorrowRepository;
+import com.udemy.demo.configuration.MyUserDetailService;
 import com.udemy.demo.user.User;
 import com.udemy.demo.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,8 +32,8 @@ public class BookController {
     private BorrowRepository borrowRepository;
 
     @GetMapping(value = "/books")
-    public ResponseEntity listBooks(@RequestParam(required = false) BookStatus status) {
-        Integer userConnectedId = this.getUserConnectedId();
+    public ResponseEntity listBooks(@RequestParam(required = false) BookStatus status, Principal principal) {
+        Integer userConnectedId = this.getUserConnectedId(principal);
         List<Book> books;
         if (status != null && status == BookStatus.FREE) {
             books = bookRepository.findByStatusAndUserIdNotAndDeletedFalse(status, userConnectedId);
@@ -40,13 +43,19 @@ public class BookController {
         return new ResponseEntity(books, HttpStatus.OK);
     }
 
-    public static Integer getUserConnectedId() {
-        return 1;
+    public static Integer getUserConnectedId(Principal principal) {
+        if (!(principal instanceof UsernamePasswordAuthenticationToken)) {
+            throw new RuntimeException(("User not found"));
+        }
+        UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) principal;
+        Integer userId = ((MyUserDetailService.UserPrincipal) token.getPrincipal()).getUser().getId();
+
+        return userId;
     }
 
     @PostMapping(value = "/books")
-    public ResponseEntity addBook(@RequestBody @Valid Book book) {
-        Integer userConnectedId = this.getUserConnectedId();
+    public ResponseEntity addBook(@RequestBody @Valid Book book, Principal principal) {
+        Integer userConnectedId = this.getUserConnectedId(principal);
         Optional<User> user = userRepository.findById(userConnectedId);
         Optional<Category> category = categoryRepository.findById(book.getCategoryId());
         if (category.isPresent()) {
@@ -66,7 +75,7 @@ public class BookController {
     }
 
     @DeleteMapping(value = "/books/{bookId}")
-    public ResponseEntity deleteBook(@PathVariable("bookId") String bookId) {
+    public ResponseEntity deleteBook(@PathVariable("bookId") String bookId, Principal principal) {
         Optional<Book> bookToDelete = bookRepository.findById(Integer.valueOf(bookId));
 
         if (!bookToDelete.isPresent()) {
@@ -89,7 +98,7 @@ public class BookController {
     }
 
     @PutMapping(value = "/books/{bookId}")
-    public ResponseEntity updateBook(@PathVariable("bookId") String bookId, @Valid @RequestBody Book book) {
+    public ResponseEntity updateBook(@PathVariable("bookId") String bookId, @Valid @RequestBody Book book, Principal principal) {
         Optional<Book> bookToUpdate = bookRepository.findById(Integer.valueOf(bookId));
         if (!bookToUpdate.isPresent()) {
             return new ResponseEntity("Book not existing", HttpStatus.BAD_REQUEST);
@@ -102,7 +111,6 @@ public class BookController {
 
         return new ResponseEntity(bookToSave, HttpStatus.OK);
     }
-
 
     @GetMapping("/categories")
     public ResponseEntity listCategories() {
