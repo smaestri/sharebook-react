dcd4a259dd2c60619deb6df8fd82fd24b56e8ea6
Sylvain Maestri <sylvainmaestri@gmail.com>
2021-02-06 23:13:51 +0100

    Section 7 - React - Les fondamentaux
    Add book

---
+++ frontend/src/MyBooks.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Book from './Book'
+
+class MyBooks extends React.Component {
+
+  constructor() {
+    super();
+    this.state = { books: [] }
+  }
+  componentDidMount() {
+    // TODO charger mes livres
+    this.setState({
+      books: [
+        {
+          title: "asterix",
+          category: "BD",
+        },
+
+        {
+          title: "tintin",
+          category: "BD",
+        }
+      ]
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Mes livres</h1>
+        {this.state.books.length === 0 ? "Vous n'avez pas déclaré de livres" : null}
+        {this.state.books.map(book => (<div>
+          <Book title={book.title} category={book.category}></Book>
+          <button>Modifier</button>
+          <button>Modifier</button>
+        </div>))}
+        <br />
+        <button>Nouveau livre</button>
+      </div>)
+  }
+
+}
+export default MyBooks
