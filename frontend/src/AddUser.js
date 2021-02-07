b14304a3b2d6608dfc27b980e7adea067811ab18
Sylvain Maestri <sylvainmaestri@gmail.com>
2021-02-07 11:39:18 +0100

    Section 9 - Formulaires en REACT

--- frontend/src/AddUser.js
+++ frontend/src/AddUser.js
@@ -1,3 +1,65 @@
-export default function AddUser() {
-  return "addUser"
+import React from 'react'
+import { Link, withRouter } from "react-router-dom";
+
+import './AddUser.scss'
+
+class AddUser extends React.Component {
+
+  constructor() {
+    super();
+    this.state = { userData: {}, showModal: false }
+  }
+
+  handleChange = (event) => {
+    let currentState = { ...this.state.userData };
+    currentState[event.target.name] = event.target.value;
+    this.setState({ userData: currentState })
+  }
+
+  handleCloseModal = () => {
+    this.setState({ showModal: false })
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    console.log("onSubmit")
+    console.log(this.state.userData)
+
+  }
+
+  render() {
+    return (
+      <div className="add-user-container">
+        <div>
+          <h1>M'inscrire</h1>
+          <div>
+            <form onSubmit={this.onSubmit}>
+              <div>
+                <label>email</label>
+                <input name="email" type="text" className="form-control" onChange={this.handleChange} />
+              </div>
+              <div>
+                <label>nom</label>
+                <input name="lastName" type="text" className="form-control" onChange={this.handleChange} />
+              </div>
+              <div>
+                <label>prenom</label>
+                <input name="firstName" type="text" className="form-control" onChange={this.handleChange} />
+              </div>
+              <div>
+                <label>password</label>
+                <input name="password" type="password" className="form-control" onChange={this.handleChange} />
+              </div>
+              <div className="container-valid text-center">
+                <input type="submit" value="Valider" className="btn btn-primary" onChange={this.handleChange} />
+              </div>
+            </form>
+          </div>
+          <div><Link to="/">Retour à l'accueil</Link></div>
+        </div>
+      </div>
+    )
+  }
 }
+
+export default withRouter(AddUser)
--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,3 +1,59 @@
-export default function Login() {
-    return "Login"
+import React from 'react'
+import logo from './logo.jpg';
+import { Link } from "react-router-dom";
+import './Login.scss'
+
+export default class Login extends React.Component {
+
+    constructor() {
+        super();
+        this.state = { userData: {} }
+        this.handleChange = this.handleChange.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+        this.handleCloseModal = this.handleCloseModal.bind(this)
+    }
+
+    handleChange(event) {
+        let currentState = {...this.state.userData};
+        currentState[event.target.name] = event.target.value;
+        this.setState({ userData: currentState })
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        console.log("onsubmit")
+        console.log(this.state.userData)
+
+    }
+
+    handleCloseModal() {
+        this.setState({ showModal: false })
+    }
+
+    render() {
+        return (
+            <div className="login-container">
+                <div>
+                    <div>
+                        <img src={logo} alt="Logo" />
+                    </div>
+                    <div className="title">
+                        Bienvenue sur Sharebook!
+                    </div>
+                    <div className="form-container">
+                        <form onSubmit={this.onSubmit}>
+                            <span>Mail: </span>
+                            <input type="text" className="form-control" name="email" onChange={this.handleChange}></input>
+                            <span>Passsword: </span>
+                            <input type="password" className="form-control" name="password" onChange={this.handleChange}></input>
+                            <div>
+                                <input type="submit" className="btn btn-primary" value="OK" />
+                            </div>
+                        </form>
+                    </div>
+                    <div><Link to="/addUser">M'inscrire</Link></div>
+                </div>
+            </div>
+        )
+    }
 }
